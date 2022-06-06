7e1650f03ba26defe47c7202b611ff1457465bc6
larkes00 <zakirov2802@gmail.com>
2022-06-06 22:42:30 +0300

    fix

--- lab2_1/app/src/main/java/com/example/lab2_1/MainActivity.kt
+++ lab2_1/app/src/main/java/com/example/lab2_1/MainActivity.kt
@@ -13,13 +13,12 @@ import com.example.lab2_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
